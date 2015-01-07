14d2e7ef55ae5aedfe68140f1d9efa85d25651ca
Joe Flateau <joef@sidearmsports.com>
2015-01-07 14:10:13 -0500

    Launch config...

--- launch.bat
+++ launch.bat
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-4ae27e22 --count 1 --instance-type t2.medium --subnet-id subnet-30399869 --key-name Joef --user-data file://user_data.sh
+aws ec2 run-instances --image-id ami-9eaa1cf6 --count 1 --instance-type t2.medium --subnet-id subnet-30399869 --key-name Joef --user-data file://user_data.sh
--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -1,19 +1,10 @@
 class { 'graphite' :
     gr_apache_24               => true,
-    gr_web_cors_allow_from_all => true,
-
-    gr_storage_schemas         => [
-        {
-          name       => 'stats',
-          pattern    => '.*',
-          retentions => '1m:2d,15m:7d,2h:1y'
-        }
-      ],
 } ->
 
 class { 'statsd' :
   backends     => ['./backends/graphite'],
   graphiteHost => 'localhost',
-  flushInterval    => 1000,
+  flushInterval    => 10000,
   percentThreshold => [75, 90, 99],
 }
